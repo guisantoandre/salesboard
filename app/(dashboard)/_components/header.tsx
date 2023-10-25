c6e6d65a8057ca592bf54f15d62bf33ba4d841da
guisantoandre <gui.santoandre@gmail.com>
2023-10-25 11:29:58 -0300

    fixed header index

--- app/(dashboard)/_components/header.tsx
+++ app/(dashboard)/_components/header.tsx
@@ -2,7 +2,7 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 export function Header() {
    return (
-      <div className="h-[70px] fixed inset-0 flex items-center p-6 border-b w-full md:hidden bg-gray-200 dark:bg-stone-900">
+      <div className="h-[70px] fixed inset-0 z-50 flex items-center p-6 border-b w-full md:hidden bg-gray-200 dark:bg-stone-900">
          <MobileSidebar />
       </div>
    );
